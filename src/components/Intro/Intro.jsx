b378252af8e7fb3b2ce239d9850c586b3905462c
Gafrelek <gafrelek@gmail.com>
2024-11-05 21:00:58 +0100

    added media queries

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,8 +1,35 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './Intro.css';
+import fotoGF from '../../assets/fotoGF.jpg';
 
 const Intro = () => {
-  return <div></div>;
+  return (
+    <section id="intro">
+      <div className="introContent">
+        <div className="spacerFirstChild"></div>
+        <div className="spacerLastChild">
+          <span className="hello">
+            Hello,
+            <br />
+          </span>
+          <span className="introText">
+            {' '}
+            I&apos;m <span className="introName">Gregor</span>
+            <br />
+            Webdeveloper
+          </span>
+
+          <p className="introPara">
+            <br />
+            I am passionate about crafting seamless, user-friendly <br />{' '}
+            digital experiences.
+          </p>
+        </div>
+        <img src={fotoGF} alt="Gregor photo" className="bg" />
+      </div>
+    </section>
+  );
 };
 
 export default Intro;
