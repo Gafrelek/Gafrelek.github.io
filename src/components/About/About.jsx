5f7c76df61a3f96f6aaf0fee8ee916e51c657caf
Gafrelek <gafrelek@gmail.com>
2025-05-22 18:25:15 +0200

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,19 +9,20 @@ const About = () => {
         <p>
           Hi there! 👋 <br />
           <br />
-          I'm Gregor, a passionate web developer and IT consultant with a knack
-          for crafting innovative digital solutions. With years of experience
-          working at companies like Google Sweden and Klarna Bank AB, I’ve honed
-          my skills in everything from productivity engineering to HR systems
-          and IT support. Currently, I enjoy combining my technical expertise
-          with creativity to build user-friendly websites and tools that make
-          life easier for businesses and insectioniidkutso love collaborating on
-          projects that push the boundaries of what technology can do. When I’m
-          not coding or solving tech puzzles, you’ll probably find me exploring
-          new gadgets, diving into a good book, or tinkering with side projects
-          to stay ahead of the curve. Feel free to check out my portfolio, or
-          drop me a message—I’d love to connect and chat about how we can create
-          something amazing together! 😊
+          I&apos;m Gregor, a passionate web developer and IT consultant with a
+          knack for crafting innovative digital solutions. With years of
+          experience working at companies like Google Sweden and Klarna Bank AB,
+          I&apos;ve honed my skills in everything from productivity engineering
+          to HR systems and IT support. Currently, I enjoy combining my
+          technical expertise with creativity to build user-friendly websites
+          and tools that make life easier for businesses and insectioniidkutso
+          love collaborating on projects that push the boundaries of what
+          technology can do. When I&apos;m not coding or solving tech puzzles,
+          you&apos;ll probably find me exploring new gadgets, diving into a good
+          book, or tinkering with side projects to stay ahead of the curve. Feel
+          free to check out my portfolio, or drop me a message—I&apos;d love to
+          connect and chat about how we can create something amazing together!
+          😊
         </p>
       </div>
     </section>
